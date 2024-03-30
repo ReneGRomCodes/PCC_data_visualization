23403fdc17159285d58ef0f001432d31e348f7f8
ReneGRomCodes <rene.romero.2517@gmail.com>
2024-03-30 12:14:53 +0100

    Grouped countries into 3 population levels and changed color style

--- world_population.py
+++ world_population.py
@@ -1,5 +1,6 @@
 import json
 from pygal_maps_world.maps import World
+from pygal.style import LightColorizedStyle as LCS, RotateStyle as RS
 from country_codes import get_country_code
 
 
@@ -18,8 +19,24 @@ for pop_dic in pop_data:
         if code:
             cc_population[code] = population
 
-wm = World()
+# Group the countries into 3 population levels.
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_population.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop < 1000000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops_3[cc] = pop
+
+# See how many countries are in each level.
+print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+
+wm_style = RS("#336699", base_style=LCS)
+wm = World(style=wm_style)
 wm.title = "World Population in 2010, by Country"
-wm.add("2010", cc_population)
+wm.add("0-10m", cc_pops_1)
+wm.add("10m-1b", cc_pops_2)
+wm.add(">1bn", cc_pops_3)
 
 wm.render_to_file("world_population.svg")
