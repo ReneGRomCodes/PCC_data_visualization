be1006ec79ba50c2e301c65b4db76c8357f5be87
ReneGRomCodes <rene.romero.2517@gmail.com>
2024-03-30 11:25:09 +0100

    Added simple visualization of world population in 2010 to 'world_population.py'

--- world_population.py
+++ world_population.py
@@ -1,4 +1,5 @@
 import json
+from pygal_maps_world.maps import World
 from country_codes import get_country_code
 
 
@@ -7,13 +8,18 @@ filename = "population_data.json"
 with open(filename) as f:
     pop_data = json.load(f)
 
-# Print the 2010 population for each country.
+# Build a dictionary of population data.
+cc_population = {}
 for pop_dic in pop_data:
     if pop_dic["Year"] == "2010":
         country_name = pop_dic["Country Name"]
         population = int(float(pop_dic["Value"]))
         code = get_country_code(country_name)
         if code:
-            print(f"{country_name}: {population}")
-        else:
-            print(f"ERROR - {country_name}")
+            cc_population[code] = population
+
+wm = World()
+wm.title = "World Population in 2010, by Country"
+wm.add("2010", cc_population)
+
+wm.render_to_file("world_population.svg")
