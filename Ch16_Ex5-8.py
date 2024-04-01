3060f602db44fca99cfb45a2cdd55aed722ea61e
ReneGRomCodes <rene.romero.2517@gmail.com>
2024-04-01 17:35:09 +0200

    Prepared exercises for chapter 16

---
+++ Ch16_Ex5-8.py
@@ -0,0 +1,19 @@
+# Exercise 16-5 All Countries: On the population map we made in this section, our program couldn't automatically find
+# two-letter codes for about 12 countries. Work out which countries are missing codes and look through the
+# 'COUNTRIES' dictionary for the codes. Add an 'if-elif-else' block to 'get_country_code()' so it returns the
+# correct country code values for these specific countries.
+
+
+# Exercise 16-6 Gross Domestic Product: Download the JSON version of the GDP data set from the 'Open Knowledge
+# Foundation' and plot the GDP for each country in the world for the most recent year in the dataset.
+
+
+# Exercise 16-7 Choose Your Own Data: The World Bank maintains data sets that are broken down for information on each
+# country worldwide. Go to http://www.data.worldbank.org/indicator and find a data set that looks interesting and
+# download it as CSV file. Write a program that generates a dictionary with Pygal's two-letter country codes as its keys
+# and your chosen data from the file as its values, then plot the data on a 'Worldmap' and style the map as you like.
+
+
+# Exercise 16-8 Testing the 'country_codes' Module: When we wrote the 'country_codes' module, we used 'print' statements
+# to check whether the 'get_country_code()' function worked. Wrote a proper test for this function using what you
+# learned in chapter 11.
